9ecf98fbec9945b1171567b4c72fd2e4ba9ff8c3
YunusHuseyinYildirim <146729154+YunusHuseyinYildirim@users.noreply.github.com>
2023-10-02 18:53:12 +0300

    Update ipextractor.sh

--- ipextractor.sh
+++ ipextractor.sh
@@ -4,4 +4,4 @@
 #!/bin/sh
 echo "Unique IP Addresses accessing your site" 
 
-sudo grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9] \+" yht.txt | sort | uniq
+sudo grep -o "[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+" yht.txt | sort | uniq
